2e6f08b46a90937b396c573707124b907830004b
Link-Zhang <Link.Zhang.0@gmail.com>
2025-08-15 15:17:16 +0800

    dev20250815: gradle init

---
+++ sop-be/src/main/java/cn/sh/lz/Hello.java
@@ -0,0 +1,9 @@
+package cn.sh.lz;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println(StringUtils.capitalize("sop-be: Hello,Gradle."));
+    }
+}
