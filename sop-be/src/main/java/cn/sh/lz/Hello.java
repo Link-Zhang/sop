1eb1cfd851ad6e87efc8f4b3f071cb28d4f3c0a3
Link-Zhang <Link.Zhang.0@gmail.com>
2025-08-15 22:50:51 +0800

    dev20250815: use gradle to control nextjs

--- sop-be/src/main/java/cn/sh/lz/Hello.java
+++ sop-be/src/main/java/cn/sh/lz/Hello.java
@@ -4,6 +4,6 @@ import org.apache.commons.lang3.StringUtils;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println(StringUtils.capitalize("sop-be: Hello,Gradle."));
+        System.out.println(StringUtils.capitalize("sop-be: Hello,Java."));
     }
 }
