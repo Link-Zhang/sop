cf29af8960b9fff7aa5f1abd2c7f0e63e01d374b
Link Zhang <Link.Zhang.0@gmail.com>
2026-01-29 14:34:32 +0800

    dev20260129: update

--- sop-fe/src/app/todo/page.tsx
+++ sop-fe/src/app/todo/page.tsx
@@ -4,7 +4,7 @@ import TodoList from "@/app/todo/components/TodoList";
 import TodoTitle from "@/app/todo/components/TodoTitle";
 
 export default async function TodoPage() {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
+  await new Promise((resolve) => setTimeout(resolve, 500));
 
   return (
     <React.Fragment>
